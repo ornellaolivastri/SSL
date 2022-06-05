7825bc01a74f573cd1e2d3d56f3cd08561cd1110
ornella olivastri <oolivastri@frba.utn.edu.ar>
2022-06-05 18:40:57 -0300

    tp 02, fases de traduccion y errores

---
+++ 02-FasesTraduccionYErrores/hello3.s
@@ -0,0 +1,29 @@
+	.file	"hello3.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "La respuesta es %d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$42, -4(%rbp)
+	leaq	.LC0(%rip), %rcx
+	call	prontf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-sjlj-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	prontf;	.scl	2;	.type	32;	.endef
